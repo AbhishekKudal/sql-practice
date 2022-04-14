22293653be6c31e668b5d791bf0b2aedc05831e2
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2022-04-14 23:30:27 +0530

    Update SQL_basic_8.sql

--- SQL_basic_8.sql
+++ SQL_basic_8.sql
@@ -1,7 +1,7 @@
 /* Problem: Create a new table profession to hold old_prof values from another table my_contacts
 
 =======================First Way===============================
-CREATE, SELECT and INSERT at (nearly) the same time				*/
+CREATE, SELECT and INSERT at (nearly) the same time	*/
 	CREATE TABLE profession
 	(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -13,7 +13,7 @@ CREATE, SELECT and INSERT at (nearly) the same time				*/
 		ORDER BY old_prof;
 
 /*=======================Second Way===============================
-CREATE TABLE with SELECT, then ALTER to add primary key			*/
+CREATE TABLE with SELECT, then ALTER to add primary key	*/
 	CREATE TABLE profession AS
 	SELECT old_prof FROM my_contacts
 	GROUP BY old_prof
@@ -28,12 +28,12 @@ CREATE TABLE with primary key and with SELECT all in one step	*/
 	(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	new_prof VARCHAR(10)
-	)AS														--SQL auto-increments,so your RDBMS knows the id column should be fed automatically,
-	SELECT old_prof FROM my_contacts						--and that leaves only one column, which is where the data goes.
+	)AS							--SQL auto-increments,so your RDBMS knows the id column should be fed automatically,
+	SELECT old_prof FROM my_contacts			--and that leaves only one column, which is where the data goes.
 	GROUP BY old_prof
 	ORDER BY old_prof;
 	
-/*==========================CARTESIAN JOIN (CROSS JOIN)============================
+/*==========================================CARTESIAN JOIN (CROSS JOIN)============================================
 The Cartesian join takes each value in from the first table and pairs it up with each value from the second table
 The CROSS JOIN returns every row from one table crossed with every row from the second.*/
 	
@@ -58,10 +58,10 @@ The CROSS JOIN returns every row from one table crossed with every row from the 
 	SELECT toys.toy, boys.boy
 	FROM toys CROSS JOIN boys;
 
-	SELECT t.toy, b.boy           			--The name before the dot is the table, and the name after it is the name of a column
+	SELECT t.toy, b.boy           		--The name before the dot is the table, and the name after it is the name of a column
 	FROM toys AS t CROSS JOIN boys as b;	--t is table Alias for toys and b is table Alias for boys
 	
-/*=======================================INNER JOIN====================================================	
+/*=======================================================INNER JOIN======================================================================
 An INNER JOIN is a CROSS JOIN with some result rows removed by a condition in the query
 	
 	SELECT somecolumns
@@ -72,13 +72,13 @@ An INNER JOIN is a CROSS JOIN with some result rows removed by a condition in th
 	
 	An INNER JOIN combines the records from two tables using comparison operators in a condition.
 	
-1. EQUIJOIN inner joins test for equality		*/
+1. EQUIJOIN inner joins test for equality	*/
 	SELECT boys.boy, toys.toy
 	FROM boys INNER JOIN toys
 	ON boys.toy_id = toys.toy_id;		--Tests for Equality
 
 /*	
-2. NON-EQUIJOIN inner joins test for inequality		*/
+2. NON-EQUIJOIN inner joins test for inequality	*/
 	SELECT boys.boy, toys.toy
 	FROM boys INNER JOIN toys
 	WHERE boys.toy_id <> toys.toy_id;	--Tests for InEquality
@@ -86,8 +86,8 @@ An INNER JOIN is a CROSS JOIN with some result rows removed by a condition in th
 	
 /*	
 3. NATURAL JOIN inner joins identify matching column names.
-	Natural joins only work if the column you’re joining by has the 
-	same name in both tables.								*/
+
+	IMP: Natural joins only work if the column you’re joining by has the same name in both tables.	*/
 	
 	SELECT boys.boy, toys.toy
-	FROM boys NATURAL JOIN toys;		--Does not require any condition
+	FROM boys NATURAL JOIN toys;		--Does not require any ON/WHERE condition
