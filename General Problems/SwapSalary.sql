506e3128ee138a8e8dd39ad13c880706171e772a
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2022-09-15 23:56:16 +0530

    627. Swap Salary

--- General Problems/SwapSalary.sql
+++ General Problems/SwapSalary.sql
@@ -7,7 +7,7 @@
 
 # Using CASE:
 
-# UPDATE Salary SET sex = (CASE 
-#                             WHEN sex = 'm' THEN 'f'
-#                             ELSE 'm'
-#                          END);
+ UPDATE Salary SET sex = (CASE 
+                             WHEN sex = 'm' THEN 'f'
+                             ELSE 'm'
+                          END);
