3e12c9cfc1951da83d0d31d53ab018402a4877bc
Abhishek Kudal <102067485+AbhishekKudal@users.noreply.github.com>
2023-01-02 18:54:24 +0530

    182. Duplicate Emails
    https://leetcode.com/problems/duplicate-emails/description/

--- General Problems/DuplicateEmails.sql
+++ General Problems/DuplicateEmails.sql
@@ -9,3 +9,6 @@ SELECT email from Person group by email having count(email) > 1;
 
 # Similar Approach:
 select email as Email from Person group by email having count(*)>1;
+
+# Approach 2:
+SELECT DISTINCT(p1.email) from Person p1 INNER JOIN Person p2 ON p1.email = p2.email AND p1.id <> p2.id;
